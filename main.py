4c8f01541a8e4772d09c4c63cb59446277f0a070
Lukas Scheucher <scheuclu@gmail.com>
2022-06-21 11:09:33 +0700

    TODO

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import torch
 import torch.optim as optim
 import torchvision.models as models
 from model import get_style_model_and_losses
-from conf import standard_configs, corrected_configs
+from conf import standard_configs, scream_configs
 import numpy as np
 
 from torch.optim.lr_scheduler import ReduceLROnPlateau
@@ -107,7 +107,7 @@ def run_conf(conf):
 
     print(input_img)
 
-for conf in reversed(corrected_configs_configs):
+for conf in reversed(scream_configs):
     try:
         run_conf(conf)
     except:
